ae7f2bdafe0943c0ca8ae29ce6027ed37fd47c28
tommykw <destinys.lotus.1108mn@gmail.com>
2020-06-08 22:59:25 +0900

    Add react-markdown

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { useStateWithStorage } from '../hooks/use_state_with_storage'
+import * as ReactMarkdown from 'react-markdown'
 
 const Header = styled.header`
   font-size: 1.5rem;
@@ -59,7 +60,9 @@ export const Editor: React.FC = () => {
           onChange={(event) => setText(event.target.value)}
           value={text}
         />
-        <Preview>プレビューエリア</Preview>
+        <Preview>
+            <ReactMarkdown source={text} />
+        </Preview>
       </Wrapper>
     </>
   )
