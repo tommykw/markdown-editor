71b36b78b7ae645610b1ca09b8622be87e0659bf
tommykw <destinys.lotus.1108mn@gmail.com>
2020-06-08 21:53:23 +0900

    Create the outline of markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
 
-const Header = styled.h1`
-  color: red;
+const GlobalStyle = createGlobalStyle`
+   body * {
+     box-sizing: border-box;
+   }
 `
 
-const Main = (<Header>Markdown Editor</Header>)
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+)
 
 render(Main, document.getElementById('app'))
---
+++ src/pages/editor.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.header`
+  font-size: 1.5rem;
+  height: 2rem;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem 1rem;
+  position: fixed;
+  right: 0;
+  top: 0;
+`
+
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+`
+
+const TextArea = styled.textarea`
+  border-right: 1px solid silver;
+  border-top: 1px solid silver;
+  bottom: 0;
+  font-size: 1rem;
+  left: 0;
+  padding: 0.5rem;
+  position: absolute;
+  top: 0;
+  width: 50vw;
+`
+
+const Preview = styled.div`
+  border-top: 1px solid silver;
+  bottom: 0;
+  overflow-y: scroll;
+  padding: 1rem;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 50vw;
+`
+
+export const Editor: React.FC = () => {
+  return (
+    <>
+      <Header>
+        Markdown Editor
+      </Header>
+      <Wrapper>
+        <TextArea value="テキスト入力エリア" />
+        <Preview>プレビューエリア</Preview>
+      </Wrapper>
+    </>
+  )
+}
