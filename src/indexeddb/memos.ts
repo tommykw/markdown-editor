f00f2fd68633c1471b2725e332f12deda10dc66f
tommykw <destinys.lotus.1108mn@gmail.com>
2020-07-05 19:58:10 +0900

    Add dexie

---
+++ src/indexeddb/memos.ts
@@ -0,0 +1,16 @@
+import Dexie from 'dexie'
+
+export interface MemoRecord {
+    datetime: string
+    title: string
+    text: string
+}
+
+const database = new Dexie('markdown-editor')
+database.version(1).stores({ memos: '&datetime' })
+const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
+
+export const putMemo = async (title: string, text: string): Promise<void> => {
+    const datetime = new Date().toISOString()
+    await memos.put({ datetime, title, text })
+}
