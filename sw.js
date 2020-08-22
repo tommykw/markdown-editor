ba24d0bc609ab782394f89d1f5b81d2330101bae
tommykw <destinys.lotus.1108mn@gmail.com>
2020-08-22 10:28:28 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CacheName = 'Cache:v1'
+
+self.addEventListener('install', (event) => {
+    console.log('ServiceWorker install:', event)
+})
+
+self.addEventListener('activate', (event) => {
+    console.log('ServiceWorker activate:', event)
+})
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch to: ', event.request.url)
+    event.respondWith(fetch(event.request))
+})
+
+const networkFaillingBackToCache = async (request) => {
+    const cache = await caches.open(CacheName)
+    try {
+        const response = await fetch(request)
+        await cache.put(request, response.clone())
+        return response
+    } catch (err) {
+        console.error(err)
+        return cache.match(request)
+    }
+}
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(networkFaillingBackToCache(event.request))
+})
